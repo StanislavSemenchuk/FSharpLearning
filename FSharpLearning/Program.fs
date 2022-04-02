9972094fd49c0887e1f56c6ff0945b6d75e23cfe
Stanislav Semenchuk <staniswlavsemenchuk@gmail.com>
2022-04-02 12:34:02 +0300

    Initial

--- FSharpLearning/Program.fs
+++ FSharpLearning/Program.fs
@@ -1,2 +1,30 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.Runtime.InteropServices
+open System.Globalization
+
+
+type Language = {
+    LanguageIdentifier : int;
+    Language : string;
+    Locale : string;
+}
+
+
+[<DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)>]
+extern uint GetKeyboardLayoutList(int nBuff, [<Out>] IntPtr[] lpList)
+
+let GetOS : OSPlatform =
+    match int Environment.OSVersion.Platform with
+        | 4 | 128 -> OSPlatform.Linux
+        | 6       -> OSPlatform.OSX
+        | _       -> OSPlatform.Windows
+
+let GetInstalledLanguagesWindows = 
+    let size : uint = GetKeyboardLayoutList(0, null)
+    let ids : IntPtr[] = Array.zeroCreate<IntPtr> (int size)
+    GetKeyboardLayoutList(ids.Length , ids) |> ignore
+    [for id in ids do
+        let bla = int id &&& 0xFFFF
+        [yield new CultureInfo(bla)]]
+
+GetInstalledLanguagesWindows |> printf "%A"
